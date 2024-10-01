9aa8311c7bd0da5d9203dcf00783cac5713c54c1
Damola <odamola@mit.edu>
2024-10-01 17:32:30 +0100

    server setup

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./config/app.config";
+import colors from "colors";
+
+const PORT = process.env.PORT || 5006;
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+process.on('unhandledRejection', (err:any, promise) => {
+    console.log(colors.bold.red(`Error: ${err.message}`))
+    server.close(() => process.exit(1))
+})
