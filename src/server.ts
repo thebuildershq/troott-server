d84ad4212ca0b094ae09ef446ea3c50f34c60f53
Damola <odamola@mit.edu>
2024-10-03 13:24:31 +0100

    db configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import app from "./config/app.config";
 import colors from "colors";
+import connectDB from "./config/db.config";
 
+const connect = async () : Promise<void> => {
 
+    await connectDB
+}
+
+connect()
 
 
 const PORT = process.env.PORT || 5006;
-
+ 
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
