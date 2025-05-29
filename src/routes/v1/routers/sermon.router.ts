b20f84c077a5ec59eea10e382546fdea6bd17f90
Damola Oladipo <odamola@mit.edu>
2025-05-29 21:15:00 +0100

    feat: added handle publish sermon routes

--- src/routes/v1/routers/sermon.router.ts
+++ src/routes/v1/routers/sermon.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { UploadSermon } from "../../../controllers/sermon.controller";
+import { publishSermon, uploadSermon } from "../../../controllers/sermon.controller";
 
 
 const sermonRouter = Router({ mergeParams: true });
 
-sermonRouter.post("/start-upload", UploadSermon);
-
+sermonRouter.post("/start-upload", uploadSermon);
+sermonRouter.post("/finish-upload", uploadSermon);
+sermonRouter.post("/publish", publishSermon);
+sermonRouter.get("/public", uploadSermon);
+sermonRouter.get("/category/:category", uploadSermon);
 
 export default sermonRouter;
